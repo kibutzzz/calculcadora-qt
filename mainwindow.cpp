b99410854ff0441ac7587133fbb4552f77d6cb92
wagner dias <wagner@inbtelecom.com.br>
2020-06-03 21:30:05 -0300

    funcoes_botos

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,58 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btnUm_clicked()
+{
+    ui->inputNumeros->insert("1");
+}
+
+void MainWindow::on_btnDois_clicked()
+{
+    ui->inputNumeros->insert("2");
+}
+
+void MainWindow::on_btnTres_clicked()
+{
+    ui->inputNumeros->insert("3");
+}
+
+void MainWindow::on_btnQuatro_clicked()
+{
+    ui->inputNumeros->insert("4");
+}
+
+void MainWindow::on_btnCinco_clicked()
+{
+    ui->inputNumeros->insert("5");
+}
+
+void MainWindow::on_btnSeis_clicked()
+{
+    ui->inputNumeros->insert("6");
+}
+
+void MainWindow::on_btnSete_clicked()
+{
+    ui->inputNumeros->insert("7");
+}
+
+void MainWindow::on_btnOito_clicked()
+{
+    ui->inputNumeros->insert("8");
+}
+
+void MainWindow::on_btnNove_clicked()
+{
+    ui->inputNumeros->insert("9");
+}
+
+void MainWindow::on_btnZero_clicked()
+{
+    ui->inputNumeros->insert("0");
+}
+
+void MainWindow::on_btnLimpar_clicked()
+{
+    ui->inputNumeros->clear();
+}
